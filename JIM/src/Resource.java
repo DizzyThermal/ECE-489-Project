17078dec862efc75f0f9665ce977f137eaaed8d2
Simmons <Simmons@Simmons-PC>
2013-11-30 00:29:47 -0500

    Defaulting to diniz.dyndns-home.com as default host because it will work for erbody

--- JIM/src/Resource.java
+++ JIM/src/Resource.java
@@ -3,7 +3,7 @@ public class Resource
 	public static final String VERSION_NUMBER		= "v1.0.0";
 	public static final String VERSION_CODENAME		= "Clementine";
 	
-	public static String IP							= "localhost";
+	public static String IP							= "diniz.dyndns-home.com";
 	public static String PORT						= "4444";
 	public static String LISTENING_PORT				= "55555";
 	
